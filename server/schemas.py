d2d5e79adde0999bf41ad67e7d35a82953994db8
Bildad Ochieng Masaga <bijasoto@gmail.com>
2026-04-21 23:43:27 +0300

    created a schema with validations and constraints

---
+++ server/schemas.py
@@ -0,0 +1,25 @@
+from marshmallow import Schema, fields, validate
+from models import Exercise, Workout, WorkoutExercise
+
+class ExerciseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True, validate=validate.Length(min=2))
+    category = fields.Str(required=True)
+    equipment_needed = fields.Bool()
+
+class WorkoutExerciseSchema(Schema):
+    id = fields.Int(dump_only=True)
+    workout_id = fields.Int(required=True)
+    exercise_id = fields.Int(required=True)
+    reps = fields.Int(required=True, validate=validate.Range(min=1))
+    sets = fields.Int(required=True, validate=validate.Range(min=1))
+    duration_seconds = fields.Int()
+    # Nested exercise info for viewing
+    exercise = fields.Nested(ExerciseSchema, only=('name', 'category'), dump_only=True)
+
+class WorkoutSchema(Schema):
+    id = fields.Int(dump_only=True)
+    date = fields.Date(required=True)
+    duration_minutes = fields.Int(validate=validate.Range(min=1))
+    notes = fields.Str()
+    workout_exercises = fields.List(fields.Nested(WorkoutExerciseSchema), dump_only=True)
